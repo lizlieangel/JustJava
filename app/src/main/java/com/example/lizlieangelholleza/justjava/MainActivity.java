6cebcc3a3c3dab8a0e3a1add2c2e7b5941a8034b
Lizlie Angel Holleza <lizlieangelholleza@Lizlies-MacBook-Pro.local>
2018-07-15 18:03:34 +0800

    add submit order and display method in main acitivity and add the onClick attr in acitivity main

--- app/src/main/java/com/example/lizlieangelholleza/justjava/MainActivity.java
+++ app/src/main/java/com/example/lizlieangelholleza/justjava/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.lizlieangelholleza.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        display(77*2+1);
+    }
+
+    public void display(int q) {
+        TextView quantity = (TextView) findViewById(R.id.quantity_text_view);
+        quantity.setText("" + q);
     }
 }
