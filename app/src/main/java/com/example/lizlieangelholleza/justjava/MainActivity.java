261019f71b6d85e807db2ae8844a46ff375a8cd6
Lizlie <lizlieholleza@gmail.com>
2018-07-15 19:49:57 +0800

    add display price method and its attr in acitivity main:

--- app/src/main/java/com/example/lizlieangelholleza/justjava/MainActivity.java
+++ app/src/main/java/com/example/lizlieangelholleza/justjava/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,11 +16,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        display(77*2+1);
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     public void display(int q) {
         TextView quantity = (TextView) findViewById(R.id.quantity_text_view);
         quantity.setText("" + q);
+    }
+
+    public void displayPrice(int p) {
+        TextView price = (TextView) findViewById(R.id.price_text_view);
+        price.setText(NumberFormat.getCurrencyInstance().format(p));
     }
 }
